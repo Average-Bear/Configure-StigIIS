3c4fad3d83253e9fec76f68b0a61c6353314a0ab
Jeremy DeWitt <jeremyndewitt@gmail.com>
2018-09-05 12:29:15 +1200

    Update Configure-StigIIS.ps1

--- Configure-StigIIS.ps1
+++ Configure-StigIIS.ps1
@@ -19,7 +19,6 @@
 
     Reports will be output to the $ServerPath variable; you will need to set this for the desired location.
 
-rts
     Configured/Reported Vulnerabilities: 
     V-76679, V-76779, V-76781, V-76681, V-76783, V-76683, V-76785, V-76685, V-76787, V-76687, V-76689, V-76789, V-76791, V-76695, V-76697, V-76795, V-76701, V-76703, V-76707, V-76719, V-76709, V-76711, V-76797, V-76713, V-76803, 
     V-76715, V-76717, V-76725, V-76727, V-76777, V-76731, V-76733, V-76829, V-76735, V-76737, V-76835, V-76753, V-76755, V-76757, V-76855, V-76759, V-76767, V-76769, V-76771, V-76773, V-76775, V-76813, V-76805, V-76809, V-76851, 
